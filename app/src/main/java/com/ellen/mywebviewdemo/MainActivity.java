47f115be05f972678e327acae9e804bfae8ef2c4
Ellen2018 <1961075191@qq.com>
2019-05-09 22:47:42 +0800

    Initial commit

---
+++ app/src/main/java/com/ellen/mywebviewdemo/MainActivity.java
@@ -0,0 +1,247 @@
+package com.ellen.mywebviewdemo;
+
+import android.content.DialogInterface;
+import android.net.Uri;
+import android.os.Build;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.webkit.JavascriptInterface;
+import android.webkit.JsPromptResult;
+import android.webkit.JsResult;
+import android.webkit.ValueCallback;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+
+import java.util.HashMap;
+import java.util.Set;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView webView;
+    private Button bt1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        webView = findViewById(R.id.webview);
+        bt1 = findViewById(R.id.bt1);
+        //Android调用JS方法
+        //webViewMethod1();
+        //Js调用Android方法1
+        //webViewMethod2();
+        //Js调用Android方法2
+        //webViewMethod3();
+        //Js调用Android方法3
+        webViewMethod4();
+    }
+
+    //有bug,只会调用一次。
+    private void webViewMethod4() {
+
+        WebSettings webSettings = webView.getSettings();
+
+        // 设置与Js交互的权限
+        webSettings.setJavaScriptEnabled(true);
+        // 设置允许JS弹窗
+        webSettings.setJavaScriptCanOpenWindowsAutomatically(true);
+
+// 先加载JS代码
+        // 格式规定为:file:///android_asset/文件名.html
+        webView.loadUrl("file:///android_asset/jsdemo_4.html");
+
+
+        webView.setWebChromeClient(new WebChromeClient() {
+                                        // 拦截输入框(原理同方式2)
+                                        // 参数message:代表promt（）的内容（不是url）
+                                        // 参数result:代表输入框的返回值
+                                        @Override
+                                        public boolean onJsPrompt(WebView view, String url, String message, String defaultValue, JsPromptResult result) {
+                                            // 根据协议的参数，判断是否是所需要的url(原理同方式2)
+                                            // 一般根据scheme（协议格式） & authority（协议名）判断（前两个参数）
+                                            //假定传入进来的 url = "js://webview?arg1=111&arg2=222"（同时也是约定好的需要拦截的）
+
+                                            Uri uri = Uri.parse(message);
+                                            // 如果url的协议 = 预先约定的 js 协议
+                                            // 就解析往下解析参数
+                                            if ( uri.getScheme().equals("js")) {
+
+                                                // 如果 authority  = 预先约定协议里的 webview，即代表都符合约定的协议
+                                                // 所以拦截url,下面JS开始调用Android需要的方法
+                                                Log.e("标记",uri.getAuthority());
+                                                if (uri.getAuthority().equals("demo")) {
+
+                                                    //
+                                                    // 执行JS所需要调用的逻辑
+                                                   Log.e("标记","js调用了Android的方法");
+                                                    // 可以在协议上带有参数并传递到Android上
+                                                    HashMap<String, String> params = new HashMap<>();
+                                                    Set<String> collection = uri.getQueryParameterNames();
+
+                                                    //参数result:代表消息框的返回值(输入值)
+                                                    result.confirm("js调用了Android的方法成功啦");
+                                                }
+                                                return true;
+                                            }
+                                            return super.onJsPrompt(view, url, message, defaultValue, result);
+                                        }
+
+                                        // 通过alert()和confirm()拦截的原理相同，此处不作过多讲述
+
+                                        // 拦截JS的警告框
+                                        @Override
+                                        public boolean onJsAlert(WebView view, String url, String message, JsResult result) {
+                                            return super.onJsAlert(view, url, message, result);
+                                        }
+
+                                        // 拦截JS的确认框
+                                        @Override
+                                        public boolean onJsConfirm(WebView view, String url, String message, JsResult result) {
+                                            return super.onJsConfirm(view, url, message, result);
+                                        }
+                                    }
+        );
+
+
+
+    }
+
+    private void webViewMethod3() {
+
+        WebSettings webSettings = webView.getSettings();
+
+        // 设置与Js交互的权限
+        webSettings.setJavaScriptEnabled(true);
+        // 设置允许JS弹窗
+        webSettings.setJavaScriptCanOpenWindowsAutomatically(true);
+
+        // 步骤1：加载JS代码
+        // 格式规定为:file:///android_asset/文件名.html
+        webView.loadUrl("file:///android_asset/jsdemo_3.html");
+
+
+// 复写WebViewClient类的shouldOverrideUrlLoading方法
+        webView.setWebViewClient(new WebViewClient() {
+                                      @Override
+                                      public boolean shouldOverrideUrlLoading(WebView view, String url) {
+
+                                          // 步骤2：根据协议的参数，判断是否是所需要的url
+                                          // 一般根据scheme（协议格式） & authority（协议名）判断（前两个参数）
+                                          //假定传入进来的 url = "js://webview?arg1=111&arg2=222"（同时也是约定好的需要拦截的）
+
+                                          Uri uri = Uri.parse(url);
+                                          // 如果url的协议 = 预先约定的 js 协议
+                                          // 就解析往下解析参数
+                                          if ( uri.getScheme().equals("js")) {
+
+                                              // 如果 authority  = 预先约定协议里的 webview，即代表都符合约定的协议
+                                              // 所以拦截url,下面JS开始调用Android需要的方法
+                                              if (uri.getAuthority().equals("webview")) {
+
+                                                  //  步骤3：
+                                                  // 执行JS所需要调用的逻辑
+                                                 Log.e("标记","js调用了Android的方法");
+                                                  // 可以在协议上带有参数并传递到Android上
+                                                  HashMap<String, String> params = new HashMap<>();
+                                                  Set<String> collection = uri.getQueryParameterNames();
+
+                                              }
+
+                                              return true;
+                                          }
+                                          return super.shouldOverrideUrlLoading(view, url);
+                                      }
+                                  }
+        );
+    }
+
+    private void webViewMethod2() {
+
+        WebSettings webSettings = webView.getSettings();
+
+        // 设置与Js交互的权限
+        webSettings.setJavaScriptEnabled(true);
+
+        // 通过addJavascriptInterface()将Java对象映射到JS对象
+        //参数1：Javascript对象名
+        //参数2：Java对象名
+        webView.addJavascriptInterface(new AndroidtoJs(), "test");//AndroidtoJS类对象映射到js的test对象
+
+        // 加载JS代码
+        // 格式规定为:file:///android_asset/文件名.html
+        webView.loadUrl("file:///android_asset/jsdemo_2.html");
+
+
+    }
+
+    static class AndroidtoJs extends Object{
+        // 定义JS需要调用的方法
+        // 被JS调用的方法必须加入@JavascriptInterface注解
+        @JavascriptInterface
+        public void hello(String msg) {
+            Log.e("标记","JS调用了Android的hello方法");
+        }
+    }
+
+    private void webViewMethod1() {
+
+        WebSettings webSettings = webView.getSettings();
+
+        // 设置与Js交互的权限
+        webSettings.setJavaScriptEnabled(true);
+        // 设置允许JS弹窗
+        webSettings.setJavaScriptCanOpenWindowsAutomatically(true);
+
+        // 先载入JS代码
+        // 格式规定为:file:///android_asset/文件名.html
+        webView.loadUrl("file:///android_asset/jsdemo1.html");
+
+        // 由于设置了弹窗检验调用结果,所以需要支持js对话框
+        // webview只是载体，内容的渲染需要使用webviewChromClient类去实现
+        // 通过设置WebChromeClient对象处理JavaScript的对话框
+        //设置响应js 的Alert()函数
+        webView.setWebChromeClient(new WebChromeClient() {
+            @Override
+            public boolean onJsAlert(WebView view, String url, String message, final JsResult result) {
+                AlertDialog.Builder b = new AlertDialog.Builder(MainActivity.this);
+                b.setTitle("Alert");
+                b.setMessage(message);
+                b.setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        result.confirm();
+                    }
+                });
+                b.setCancelable(false);
+                b.create().show();
+                return true;
+            }
+
+        });
+
+        bt1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Android调用JS方式1
+                //webView.loadUrl("javascript:callJS()");
+
+                //Android调用JS方式2(4.4之后)
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+                    webView.evaluateJavascript("javascript:callJS()", new ValueCallback<String>() {
+                        @Override
+                        public void onReceiveValue(String value) {
+                            //此处为 js 返回的结果
+                        }
+                    });
+                }
+            }
+        });
+
+    }
+}
